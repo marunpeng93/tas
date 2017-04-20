3fc202fdda706d0e65e49d6b2a1964245d3af3e7
Owen Luke <hi.owen.luke@gmail.com>
2017-04-20 23:07:57 +0800

    update for checking node in windows

--- examples/__lib/tester.js
+++ examples/__lib/tester.js
@@ -6,7 +6,7 @@
  */
 
 // Thanks David Calhoun: http://davidbcalhoun.com/2014/what-is-amd-commonjs-and-umd/
-(function(r,f){if(typeof process==='object'&&process.title.indexOf("node")>=0){module.exports=f(r);return;}if(typeof define==='function'&&define.amd){define(function(){return f(r,r.document)});return}if(typeof exports==='object'){module.exports=r.document?f(r,r.document):function(w){return f(w,w.document)};return}f(r,r.document)}(typeof window!=="undefined"?window:this,function(window, document){
+(function(r,f){if(typeof process==='object'&&process.platform!=='undefined'){module.exports=f(r);return;}if(typeof define==='function'&&define.amd){define(function(){return f(r,r.document)});return}if(typeof exports==='object'){module.exports=r.document?f(r,r.document):function(w){return f(w,w.document)};return}f(r,r.document)}(typeof window!=="undefined"?window:this,function(window, document){
 
 	var tester = {
 		data: [],
