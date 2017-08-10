eadcf12a15a624e17c62596ba70b15fe3567262e
Owen Luke <hi.owen.luke@gmail.com>
2017-08-10 15:51:19 +0800

    remove comments

--- main.js
+++ main.js
@@ -5,12 +5,4 @@
  * Released under the MIT License.
  */
 
-// ----------------------------------------
-// Run the examples:
-//    $ node examples/nodejs/__tas/test.js
-//
-// For debugging with the examples, uncomment the below line:
-//    require('./examples');
-// ----------------------------------------
-
 module.exports = require('./lib');
