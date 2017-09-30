95a0190dc475845909f4cfe68923eaecdef5471c
Owen Luke <hi.owen.luke@gmail.com>
2017-10-01 00:26:04 +0800

    add return "breakForEach" and tas.breakForEach()

---
+++ examples/usage/web/js/src/4.forEach_tasks/return_breakForEach.js
@@ -0,0 +1,42 @@
+/**
+ * Test of Tas.js
+ * (c) 2017 Owen Luke
+ * https://github.com/tasjs/tas
+ * Released under the MIT License.
+ */
+
+// tas.race() is an extension of Tas, we need to load it at the first.
+var tas = require('../tas').load('forEach');
+
+var a = 0;
+
+tas(function(){
+	var arr = [1, 2];
+	return [arr];
+});
+
+// Perform a set of tasks for each array element
+tas.forEach({
+	init: function(element){
+		if (element === 1) {
+			a = 2;
+
+			// Break tas.forEach(), go to the next task
+			return "breakForEach";
+		}
+	},
+
+	calc: function(){ // ignored
+		a ++;
+	}
+});
+
+tas(function(){
+	a ++; // 3
+});
+
+module.exports = {
+	get: function(){
+		return a; // 3
+	}
+};
